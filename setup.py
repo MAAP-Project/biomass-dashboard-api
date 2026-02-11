d026d4c5faae19956ee1d2f1f4a4879c593873bd
Henry Rodman <henry.rodman@gmail.com>
2026-02-11 12:00:37 -0600

    merge main to staging (#27)
    * feat: allow dataset source to contain an array of sources (#26)
    
    * fix: keep installed versions of boto3 and botocore

--- setup.py
+++ setup.py
@@ -22,14 +22,15 @@ inst_reqs = [
     "mercantile",
     "pyyaml~=5.4.0",
     "cachetools",
+    "pydantic<2",
 ]
 extra_reqs = {
-    "dev": ["pre-commit", "safety", "bandit"],
+    "dev": ["pre-commit", "safety", "bandit", "mypy==1.13.0"],
     "server": ["uvicorn", "click==7.0"],
     "deploy": [
-        "aws-cdk-lib>=2.0.0",
+        "aws-cdk-lib==2.91.0",
         "aws-cdk.aws_apigatewayv2_alpha>=2.67.0a0",
-        "aws-cdk.aws_apigatewayv2_integrations_alpha>=2.67.0a0"
+        "aws-cdk.aws_apigatewayv2_integrations_alpha>=2.67.0a0",
     ],
     "test": ["moto[iam]", "mock", "pytest", "pytest-cov", "pytest-asyncio", "requests"],
 }
